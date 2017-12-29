9fbf483ce977199c5ca32a060110bc09a0abd256
Dennis Majvall <dennis.majvall.sp13@thegameassembly.com>
2017-12-29 23:41:05 +0100

    Added price to Header

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # navcoin-updater
-A NavCoin value updater for the subreddit www.reddit.com/r/navcoin
+A Nav Coin value updater for the offical Nav Coin subreddit (www.reddit.com/r/navcoin)
+
+# Installation:
+1. Clone the repository
+2. Navigate to the project's directory
+3. `npm install`
+4. Start the application with `node app`
 
 # Technical specifications:
-Node.JS, Express.JS, jQuery
+- Node.js
+- not-an-aardvark's Snoowrap Reddit API Wrapper for Node (https://github.com/not-an-aardvark/snoowrap)
+- tiaanduplessis's Coin Market Cap API wrapper for Node (https://github.com/tiaanduplessis/coinmarketcap-api)
+
+# Collaborators:
+- Karzan Botani (https://github.com/botanki)
+- Dennis Majvall (https://github.com/DennisMajvall)
--- app.js
+++ app.js
@@ -1,26 +1,60 @@
-const express               = require('express'),
-      app                   = express(),
-      CoinMarketCap         = require('coinmarketcap-api'),
-      client                = new CoinMarketCap();
+const coinmarketcap		= new (require('coinmarketcap-api'))(),
+      asleep					= require('asleep'),
+			puppeteer				= require('puppeteer'),
+			credentials			= require('./credentials.json');
 
 /* global variables */
-let nav;
+const subreddit = 'testerhester123';
+let btc, usd;
 
-/* REST */
-app.get('/nav', (req,res) => {
-  res.send(nav);
-});
-
-/* fetch top 100 coins from Coinmarketcap */
+/* fetch Nav Coin BTC and USD values */
 function getNav() {
-  client.getTicker({limit: 1, currency: 'nav-coin'}).then((res) => {
-    nav = res;
-    console.log(nav);
+  return coinmarketcap.getTicker({limit: 1, currency: 'nav-coin'}).then((res) => {
+    btc = res[0].price_btc;
+    usd = res[0].price_usd;
+    usd = usd.substr(0,5);
   }).catch(console.error);
 }
 
-getNav();
-setInterval(getNav, 1000 * 60 * 15);
+async function updateCSS() {
+  const browser = await puppeteer.launch().catch(console.error),
+				page 	  = await browser.newPage().catch(console.error);
 
-app.use(express.static(__dirname + '/www', {extensions:['html']}));
-app.listen(3020, () => console.log('UP AND RUNNING ON PORT 3020!'));
+	// Start asynchronous currency fetching
+	let waitForCurrencies = getNav();
+
+	await page.goto(`https://www.reddit.com/r/${subreddit}/about/stylesheet/`).catch(console.error);
+
+	// LOGIN BRAH
+	await page.evaluate((user) => {
+		$('#login_login-main input[name="user"]').val(user.username);
+		$('#login_login-main input[name="passwd"]').val(user.password);
+		$('#login_login-main button[type="submit"]').click();
+  }, credentials).catch(console.error);
+
+	await asleep(2000); // change to 5 later, brah
+
+	// GET CSS, BRAH
+	let css = await page.evaluate(() => $('#stylesheet_contents').val()).catch(console.error);
+
+	// make sure the Currency has finished fetching
+	await waitForCurrencies;
+
+	if (!usd || !btc) { browser.close(); return; }
+
+	css = css.replace(/(content: ")(.*)(";\/\*custom-header-price-node\*\/)/, '$1' + 'USD: ' + usd + ' BTC: ' + btc + '$3');
+
+	// UPDATE CSS, YO
+	await page.evaluate((style) => {
+		$('#stylesheet_contents').val(style);
+		$('input[name="reason"]').val('BOT: auto-update price in header.');
+		$('.sheets button[name="save"]').click();
+	}, css).catch(console.error);
+
+  await browser.close().catch(console.error);
+};
+
+/* Update the CSS at startup */
+updateCSS();
+/* update prices every 10 minutes */
+setInterval(updateCSS, 1000 * 60 * 10);
